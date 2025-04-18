1cb9f4e230535922a18067ae08fa293127ab1853
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-19 04:28:44 +0900

    feat: App 컴포넌트에서 ThemeProvider 적용 및 글로벌 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Layout from "./components/layout/Layout";
+
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { light } from "./styles/theme";
 
 function App() {
-    return <Layout children={<Home />}></Layout>;
+    return (
+        <ThemeProvider theme={light}>
+            <GlobalStyle />
+            <Layout>
+                <Home />
+            </Layout>
+        </ThemeProvider>
+    );
 }
 
 export default App;

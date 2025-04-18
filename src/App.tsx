29541214540232ec00ceb46f9ac70c998dffb58a
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-19 04:43:31 +0900

    feat: GlobalStyle에 themeName prop 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Layout from "./components/layout/Layout";
 
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
-import { light } from "./styles/theme";
+import { dark, light } from "./styles/theme";
 
 function App() {
     return (
         <ThemeProvider theme={light}>
-            <GlobalStyle />
+            <GlobalStyle themeName="light"/>
             <Layout>
                 <Home />
             </Layout>

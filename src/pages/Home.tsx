f902e11a1eb1513380b179cd9a2928b3e7f06f01
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-18 18:54:58 +0900

    feat: Header, Footer 컴포넌트 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,7 @@
-import Header from "../components/common/Header";
-import Footer from "../components/common/Footer";
-
 function Home() {
     return (
         <>
-            <Header />
             <div>home body</div>
-            <Footer />
         </>
     );
 }

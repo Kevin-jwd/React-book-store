0431b72d26b11d27dd0983d5f68f3eab82979dc5
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-21 03:42:33 +0900

    feat: Button 컴포넌트 추가및 Title 컴포넌트에 color props 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import Title from "../components/common/Title";
+import Button from "../components/common/Button";
 
 function Home() {
     return (
         <>
-            <Title size="large">제목 테스트</Title>
+            <Title size="large" color="background">제목 테스트</Title>
+            <Button size="large" scheme="normal">
+                버튼 테스트
+            </Button>
             <div>home body</div>
         </>
     );

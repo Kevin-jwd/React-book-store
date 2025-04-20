1ffac764c08bac4cff3db7fa605beb135be352bb
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-21 03:40:33 +0900

    refactor: container 대신 screen.getByText 사용

--- src/components/common/Title.spec.tsx
+++ src/components/common/Title.spec.tsx
@@ -16,17 +16,17 @@ describe("Title 컴포넌트 테스트", () => {
     });
 
     it("size props 적용", () => {
-        const { container } = render(
+        render(
             <BookStoreThemeProvider>
                 <Title size="large">제목</Title>
             </BookStoreThemeProvider>
         );
-
-        expect(container?.firstChild).toHaveStyle("font-size: 2rem;");
+    
+        expect(screen.getByText("제목")).toHaveStyle("font-size: 2rem;");
     });
 
     it("color props 적용", () => {
-        const { container } = render(
+        render(
             <BookStoreThemeProvider>
                 <Title size="large" color="primary">
                     제목
@@ -34,6 +34,6 @@ describe("Title 컴포넌트 테스트", () => {
             </BookStoreThemeProvider>
         );
 
-        expect(container?.firstChild).toHaveStyle("color: brown;");
+        expect(screen.getByText("제목")).toHaveStyle("color: brown;");
     });
 });

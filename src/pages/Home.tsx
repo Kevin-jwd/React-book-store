b60a56f2ea2a01ad7761623afde7d7b3a50289c9
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-21 16:39:19 +0900

    feat: InputText 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Title from "../components/common/Title";
 import Button from "../components/common/Button";
+import InputText from "../components/common/InputText";
 
 function Home() {
     return (
@@ -8,6 +9,7 @@ function Home() {
             <Button size="large" scheme="normal">
                 버튼 테스트
             </Button>
+            <InputText placeholder="여기에 입력하세요"></InputText>
             <div>home body</div>
         </>
     );

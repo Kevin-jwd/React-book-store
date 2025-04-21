b14ef4359171f6ea5452f61afce67e40520dbeb3
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-22 01:58:44 +0900

    style: Layout 컴포넌트에 스타일 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import styled from "styled-components";
+import { Theme } from "../../styles/theme";
 
 interface LayooutProps {
     children: React.ReactNode;
@@ -7,12 +9,19 @@ interface LayooutProps {
 
 function Layout({ children }: LayooutProps) {
     return (
-        <>
+        <LayoutStyle>
             <Header />
             <main>{children}</main>
             <Footer />
-        </>
+        </LayoutStyle>
     );
 }
+
+const LayoutStyle = styled.main<{ theme: Theme }>`
+    width: 100%;
+    margin: 0 auto;
+    max-width: ${({ theme }) => theme.layout.width.large};
+    padding: 20px 0;
+`;
 
 export default Layout;

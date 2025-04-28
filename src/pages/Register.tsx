f7121de2a9c615ba9d53a24efd51a5eea24a8350
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-28 16:06:27 +0900

    feat: 회원가입 페이지 입력 폼 개선 및 비밀번호 초기화 링크 추가

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import Title from "../components/common/Title";
 import InputText from "../components/common/InputText";
 import Button from "../components/common/Button";
+import { Link } from "react-router-dom";
+import { Theme } from "../styles/theme";
 
 function Register() {
     return (
@@ -10,20 +12,49 @@ function Register() {
             <RegisterStyle>
                 <form>
                     <fieldset>
-                        <InputText placeholder="이메일" />
+                        <InputText placeholder="이메일" inputType="email"/>
                     </fieldset>
                     <fieldset>
-                        <InputText placeholder="비밀번호" />
+                        <InputText placeholder="비밀번호" inputType="password"/>
                     </fieldset>
                     <fieldset>
-                        <Button size="medium" scheme="primary">회원가입</Button>
+                        <Button size="medium" scheme="primary">
+                            회원가입
+                        </Button>
                     </fieldset>
+                    <div className="info">
+                        <Link to="/reset">비밀번호 초기화</Link>
+                    </div>
                 </form>
             </RegisterStyle>
         </>
     );
 }
 
-const RegisterStyle = styled.div``;
+const RegisterStyle = styled.div`
+    max-width: ${({ theme }) => (theme as Theme).layout.width.small};
+    margin: 80px auto;
+
+    fieldset {
+        border: 0;
+        padding: 0 0 8px 0;
+        .error-text {
+            color: red;
+        }
+    }
+
+    input {
+        width: 100%;
+    }
+
+    Button {
+        width: 100%;
+    }
+
+    .info {
+        text-align: center;
+        padding: 16px 0 0 0;
+    }
+`;
 
 export default Register;

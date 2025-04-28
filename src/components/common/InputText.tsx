b8b67cebf78169c784e7e550446eaea416872fd5
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-04-28 16:05:26 +0900

    refactor: InputText 컴포넌트 확장성 개선
    - inputType prop 추가 (text, email, password, number)

--- src/components/common/InputText.tsx
+++ src/components/common/InputText.tsx
@@ -2,22 +2,24 @@ import React, { ForwardedRef } from "react";
 import styled from "styled-components";
 import { Theme } from "../../styles/theme";
 
-interface Props {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+    inputType?: "text" | "email" | "password" | "number";
     placeholder?: string;
 }
 
 const InputText = React.forwardRef(
-    ({ placeholder }: Props, ref: ForwardedRef<HTMLInputElement>) => {
+    ({ placeholder, inputType }: Props, ref: ForwardedRef<HTMLInputElement>) => {
         return (
             <InputTextStyle
                 placeholder={placeholder}
                 ref={ref}
+                type = {inputType}
             ></InputTextStyle>
         );
     }
 );
 
-const InputTextStyle = styled.input.attrs({ type: "text" })`
+const InputTextStyle = styled.input`
     padding: 0.25rem 0.75rem;
     border: 1px solid ${({ theme }) => (theme as Theme).color.border};
     border-radius: ${({ theme }) => (theme as Theme).borderRadius.default};
